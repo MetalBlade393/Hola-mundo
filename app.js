fa61d591a76b7f4076c68c8a63300e6fe910a2df
Metal PC <MetalBlade393@hotmail.com>
2022-04-19 15:34:15 -0400

    otra foto

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '/resources/images')));
+app.use(express.static(path.join(__dirname, 'resources')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
