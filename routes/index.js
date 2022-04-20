4062157fce557450b138ec0aba7ee7e2b72fa53b
Metal PC <MetalBlade393@hotmail.com>
2022-04-19 20:42:54 -0400

    curricullum final

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '_' });
+  res.render('index', { title: 'Curricullum' });
 });
 
 module.exports = router;
